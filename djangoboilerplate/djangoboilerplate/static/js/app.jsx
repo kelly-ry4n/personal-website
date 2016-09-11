ed00b003f73b3da1d7bdfb83b236022161eafab6
Kelly Ryan <kelly.ry4n@gmail.com>
2016-09-10 23:35:56 -0400

    clean up from template

--- djangoboilerplate/djangoboilerplate/static/js/app.jsx
+++
@@ -1,8 +0,0 @@
-var React = require('react')
-require('../scss/example/example.scss')
-
-module.exports = React.createClass({
-   render: function(){
-       return <h1>Hello, world. now with more changes!</h1>
-   }
-})
